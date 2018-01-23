09cc28d352b92ac28f0c74f663e63af61ada6b9a
Jose Luis <chefolc@gmail.com>
2018-01-22 22:43:03 -0500

    Error al registrr nota-resuelto

--- EToolWeb/FrmCargaRapidaRegistrar.aspx.cs
+++ EToolWeb/FrmCargaRapidaRegistrar.aspx.cs
@@ -89,6 +89,7 @@ namespace EToolWeb
             _CapacitacionBE.IdEtto = ddlEntrenamiento.SelectedValue;
             _CapacitacionBE.Legajo = int.Parse(ddlEmpleado.SelectedValue);
             _CapacitacionBE.Fecha = Convert.ToDateTime(txtFecha.Text);
+            _CapacitacionBE.Nota = int.Parse(txtNota.Text);
             _CapacitacionBE.Certificado = chbCertificado.Checked;
             _CapacitacionBE.Examen = msj;
             if(_CapacitacionBL.InsertarCapacitacion(_CapacitacionBE)>0)
